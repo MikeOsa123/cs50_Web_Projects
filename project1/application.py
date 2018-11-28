f9f300a459d3d1f69c44d441575f5c6ddae99c5b
Michael Osamwonyi <m.osamwonyi@gmail.com>
2018-11-28 15:21:30 +0000

    Made some HTML structural changes to Register and login page

--- project1/application.py
+++ project1/application.py
@@ -4,12 +4,23 @@ from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 
 app = Flask(__name__)
+#Secret Key needed to use CRSF
+app.config['SECRET_KEY'] = 'MikeOsa123'
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=80)])
+    remember = BooleanField('remember me')
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
+
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -25,10 +36,12 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     return render_template("register.html")
 
 @app.route("/login")
 def login():
+    form = LoginForm()
+
     return render_template("login.html")
