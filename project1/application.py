743c4553585f068e9b75a50cd36b015cce89d314
Michael Osamwonyi <michael.osamwonyi@g116806mac.thisisglobal.com>
2018-11-29 09:58:36 +0000

    Routes for Register and Login complete, users can register and login I used wtforms to do this

--- project1/application.py
+++ project1/application.py
@@ -1,21 +1,15 @@
 import os
 
-from flask import Flask, session, render_template, request
+from flask import Flask, session, render_template, request, flash, redirect, url_for
+from forms import RegistrationForm, LoginForm
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
+
 
 app = Flask(__name__)
-#Secret Key needed to use CRSF
-app.config['SECRET_KEY'] = 'MikeOsa123'
 
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=80)])
-    remember = BooleanField('remember me')
+app.config['SECRET_KEY'] = '727be3890612929864c8ed60b06d361b'
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -38,10 +32,39 @@ def index():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    form = RegistrationForm()
 
-@app.route("/login")
+    if form.is_submitted():
+        print("submitted")
+
+    if form.validate():
+        print("valid")
+
+    print(form.errors)
+
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for("index"))
+
+
+    return render_template("register.html", form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
 
-    return render_template("login.html")
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You are now logged in!', 'success')
+            return redirect(url_for("index"))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+
+    return render_template("login.html", form=form)
+
+    if __name__ == '__main__':
+        app.run(debug=true)
+
+@app.route("/about")
+def about():
+    return render_template("index.html")
