2b6204672b41024be2261dae2bda611c4d9873e8
Michael Osamwonyi <m.osamwonyi@gmail.com>
2018-12-14 17:00:45 +0000

    minor changes

--- project1/application.py
+++ project1/application.py
@@ -4,11 +4,35 @@ from flask import Flask, session, render_template, request, flash, redirect, url
 from forms import RegistrationForm, LoginForm
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from rauth.service import OAuth1Service, OAuth1Session
+import json
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '727be3890612929864c8ed60b06d361b'
+
+# consumer key & secret from: https://www.goodreads.com/api/keys
+CONSUMER_KEY = 'EETgcvhVNUCW6Xd7fCRBQ'
+CONSUMER_SECRET = '6RvW9pjgDxhUgZGOeYORhTpdqd4eODdcMTRtgmAneg'
+
+goodreads = OAuth1Service(
+    consumer_key=CONSUMER_KEY,
+    consumer_secret=CONSUMER_SECRET,
+    name='goodreads',
+    request_token_url='https://www.goodreads.com/oauth/request_token',
+    authorize_url='https://www.goodreads.com/oauth/authorize',
+    access_token_url='https://www.goodreads.com/oauth/access_token',
+    base_url='https://www.goodreads.com/'
+    )
+
+# head_auth=True is important here; this doesn't work with oauth2 for some reason
+request_token, request_token_secret = goodreads.get_request_token(header_auth=True)
+
+authorize_url = goodreads.get_authorize_url(request_token)
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = os.getenv("SECRET_KEY")
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -115,3 +139,30 @@ def about():
 @app.route("/books")
 def books():
     return render_template("book_page.html")
+
+@app.route('/<isbn_variable>', methods=['Get', 'POST'])
+def book(isbn_variable):
+    """Creates a Book page for a specific book by ISBN"""
+    # searches for book information in the database
+    book = db.execute("Select * from books where isbn = '" + isbn_variable + "'").fetchall()
+    # makes API call to Goodreads for needed information
+    APIcall = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": CONSUMER_KEY, "isbns": isbn_variable})
+    stuff = APIcall.json()
+    avg_rating = stuff['books'][0]['average_rating']
+    ratings_count = stuff['books'][0]['ratings_count']
+    # searches for reviews in our review database
+    reviews = db.execute("Select * from reviews where isbn = '" + isbn_variable + "'").fetchall()
+    # adds review for user if they haven't posted one yet. 
+    if request.method == 'POST':
+        user = session['username']
+        stars = request.form['stars']
+        review = request.form['review']
+        try:
+            db.execute('INSERT into reviews (username, isbn, stars, review) VALUES (:username, :isbn, :stars, :review)',
+            {"username": user, "isbn": isbn_variable, "stars": stars, "review": review})
+            db.commit()
+            flash('Review Submitted')
+        except:
+            flash("You've already submitted a review for this book")
+        return render_template('result.html', book=book, reviews=reviews, avg_rating=avg_rating, ratings_count=ratings_count)
+    return render_template('result.html', book=book, reviews=reviews, avg_rating=avg_rating, ratings_count=ratings_count)
